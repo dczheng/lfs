8f8196bbf3bbb35c5e4a1b435da079b6739ab0a9
dczheng <dczheng21@outlook.com>
2021-06-02 17:03:40 +0800

    update

--- tools/vim.sh
+++ tools/vim.sh
@@ -6,8 +6,8 @@ zux-get \
 && ./configure --prefix=/usr \
 && make $mkopt \
 && make install \
-&& ln -sv vim /usr/bin/vi \
+&& ln -sfv vim /usr/bin/vi \
 && for L in  /usr/share/man/{,*/}man1/vim.1; do
-        ln -sv vim.1 $(dirname $L)/vi.1
+        ln -sfv vim.1 $(dirname $L)/vi.1
 done \
-&& ln -sv ../vim/vim82/doc /usr/share/doc/vim-8.2.1361
+&& ln -sfv ../vim/vim82/doc /usr/share/doc/vim-8.2.1361
